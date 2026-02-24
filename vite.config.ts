5e2cd2821d78bef580170fc262322aede36b0508
xiaminxi <37994820+xiaminxi@users.noreply.github.com>
2026-02-24 12:42:21 +0800

    更改

--- vite.config.ts
+++ vite.config.ts
@@ -6,36 +6,48 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
 
 
+
 export default defineConfig({
-	plugins: [
-		vue(),
-		vueJsx(),
-		AutoImport({
-			imports: [
-				'vue',
-				'vue-router',
-				'pinia',
-				{
-					axios: [['default', 'axios']]
-				}
-			],
-			dts: 'src/auto-imports.d.ts',
-			eslintrc: {
-				enabled: true,
-				filepath: './.eslintrc-auto-import.json',
-				globalsPropValue: true
-			}
-		})
-	],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, 'src'),
-		},
-	},
-	server: {
-		port: 81,
-		host: true,
-		open: true,
-		proxy: { ...createProxy() }
-	}
+    define: {
+        process: process
+    },
+    plugins: [
+        vue(),
+        vueJsx(),
+        AutoImport({
+            imports: [
+                'vue',
+                'vue-router',
+                'pinia',
+                {
+                    axios: [['default', 'axios']]
+                }
+            ],
+            dts: 'src/auto-imports.d.ts',
+            eslintrc: {
+                enabled: true,
+                filepath: './.eslintrc-auto-import.json',
+                globalsPropValue: true
+            }
+        }),
+
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@use '@/style/variables.scss' as *;`
+            }
+        }
+    },
+    server: {
+        port: 81,
+        host: true,
+        open: true,
+        proxy: { ...createProxy() }
+    }
 })
